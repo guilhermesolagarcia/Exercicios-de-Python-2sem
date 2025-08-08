5bfaee14d0ab1e53a7207247720c2bd3effbaa46
guilhermesolagarcia <guilhermesolagarcia31@gmail.com>
2025-08-08 10:55:27 -0300

    Anotando o enunciado completo

--- aula-1/exercicio-1.py
+++ aula-1/exercicio-1.py
@@ -1,8 +1,26 @@
 '''
-Escreva um programa em Python, que simule o cálculo de notas de um aluno em diferentes disciplinas. O programa deve modular e utilizar as estruturas de controle e funções.
+Escreva um programa em Python, que simule o cálculo de notas de um aluno em diferentes disciplinas.
+O programa deve modular e utilizar as estruturas de controle e funções.
 
 
 1. Requisitos do Sistema
  - Função calcular_media(notas)
-  - Deve receber uma LISTA de notas como parâmetro   
+   - Deve receber uma LISTA de notas como parâmetro
+   - Deve ter uma estrutura de repetição (for ou while) para percorrer a lista
+   - retornar a média das notas (com validações)   
+ -Função verificar_aprovação(media, media_minima)
+   - Deve receber a média das notas e a média mínima para aprovação
+   - Deve usar condicionais para verificar o status do aluno
+   - Se a média for maior que a média mínima, retornar a String "Aprovado"
+   - Se a média for maior ou igual a 5.0 e menor do que a média mínima, retornar a String "Recuperação"
+   - Caso contrário, retorne a String "Reprovado"
+ - Função Principal main()
+   - Deve contar a lista com as disciplinas
+   - Média mínima para aprovação 
+   - Estrutura de repetição para iterar sobre as disciplinas
+   - Para cada disciplina, o usuário deve inserir 3 notas
+   - Chamar a função calcular_média() para obter a média da disciplina
+   - Chamar a função verificar_aprovação para obter o status do aluno
+   - Imprimir a média e o status para cada disciplina
 '''
+
