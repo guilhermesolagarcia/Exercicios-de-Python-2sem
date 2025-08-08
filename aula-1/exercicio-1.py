2a0eb18941bad9d7814dba53ea6856aaeffb94ca
guilhermesolagarcia <guilhermesolagarcia31@gmail.com>
2025-08-08 10:38:19 -0300

    Salvando o começo pra não perder

---
+++ aula-1/exercicio-1.py
@@ -0,0 +1,8 @@
+'''
+Escreva um programa em Python, que simule o cálculo de notas de um aluno em diferentes disciplinas. O programa deve modular e utilizar as estruturas de controle e funções.
+
+
+1. Requisitos do Sistema
+ - Função calcular_media(notas)
+  - Deve receber uma LISTA de notas como parâmetro   
+'''
