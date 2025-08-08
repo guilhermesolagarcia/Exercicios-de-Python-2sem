fd76bc9c1a0fc4ff9b86d8ab9ff180a2a0a8e335
guilhermesolagarcia <guilhermesolagarcia31@gmail.com>
2025-08-08 11:45:56 -0300

    Terminando a função final e chamando a Main

--- aula-1/exercicio-1.py
+++ aula-1/exercicio-1.py
@@ -38,7 +38,7 @@ def calcular_media(notas):
         soma += nota # soma = soma + nota / a soma acumula o valor de nota, no segundo laço, ele vai ter o valor da primeira nota, depois na terceira vai ter o valor da primeira + segunda / acumular valor na variável soma
 
     if len(notas) >0:
-        return soma / len(nota) # dividindo pelo tamanho da listinha de NOTAS / LEN 
+        return soma / len(notas) # dividindo pelo tamanho da listinha de NOTAS / LEN 
     else:
         return 0
     
@@ -57,3 +57,35 @@ def verificar_aprovacao(media, media_minima):
     
 
 
+def main():
+    """
+    Função principal do programa
+    - Lista com as disciplinas
+    - Média mínima de aprovação 
+    """
+
+    #lista com as disciplinas
+    disciplinas = ["Python", "Java", "Banco de Dados"]
+    media_aprovacao = 7.0
+
+    print( f'\n*--Sistema de Cálculo de Notas--*')
+    #percorre a lista de disciplinas
+    for disciplina in disciplinas:
+        print(f'Disciplina de: {disciplina}')
+
+        #lista com as notas
+        notas = []
+
+        #obter as notas por disciplina, 3 notas por disciplina
+        for i in range(3):
+            nota = float(input(f"Digite a {i+1}º nota: ")) #pegando as notas conforme a pessoa digita / o contador começa com 0 sempre, então o +1 faz ficar com a ordem certa
+            notas.append(nota) #coloca a nota na lista
+        
+        #chamar as funções calcular média e verificar aprovação
+        media_final = calcular_media(notas)
+        status = verificar_aprovacao(media_final, media_aprovacao)
+        print(f" A média final em {disciplina}: foi de {media_final:.2f}")
+        print(f"Status: {status}")
+
+#Programa Principal
+main()
